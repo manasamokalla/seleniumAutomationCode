658cb5aa98af5e1dc92313d81e6c58b629bd22b1
manasamokalla <77579021+manasamokalla@users.noreply.github.com>
2023-04-04 19:31:35 +0530

    Add files via upload

---
+++ TestAlerts.java
@@ -0,0 +1,55 @@
+package com.SeleniumPractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * How to handle alerts in a web page using Selenium WebDriver?
+ */
+public class TestAlerts {
+	public static void main(String[] args) throws Exception {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		Dimension d = new Dimension(300,500);
+		driver.manage().window().setSize(d);
+
+		driver.get("https://www.hyrtutorials.com/p/alertsdemo.html");
+
+		//Alert Box
+//				System.out.println(driver.findElement(By.id("output")).getText());
+//				driver.findElement(By.id("alertBox")).click();
+//				System.out.println(driver.switchTo().alert().getText());
+//				driver.switchTo().alert().accept();
+//				System.out.println(driver.findElement(By.id("output")).getText());
+
+		//Confirm Box
+//				System.out.println(driver.findElement(By.id("output")).getText());
+//				driver.findElement(By.id("confirmBox")).click();
+//				System.out.println(driver.switchTo().alert().getText());
+//				driver.switchTo().alert().accept();
+//				System.out.println(driver.findElement(By.id("output")).getText());
+//				driver.findElement(By.id("confirmBox")).click();
+//				System.out.println(driver.switchTo().alert().getText());
+//				driver.switchTo().alert().dismiss();
+//				System.out.println(driver.findElement(By.id("output")).getText());
+
+
+		//Prompt Box
+				System.out.println(driver.findElement(By.id("output")).getText());
+				driver.findElement(By.id("promptBox")).click();
+				System.out.println(driver.switchTo().alert().getText());
+				driver.switchTo().alert().sendKeys("HYR Tutorials");
+				driver.switchTo().alert().accept();
+				System.out.println(driver.findElement(By.id("output")).getText());
+				driver.findElement(By.id("promptBox")).click();
+				System.out.println(driver.switchTo().alert().getText());
+				driver.switchTo().alert().dismiss();
+				System.out.println(driver.findElement(By.id("output")).getText());
+
+		
+	}
+}
